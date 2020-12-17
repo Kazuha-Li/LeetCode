1ef18a8690daff5762c3c1aa701892d7037426d6
Sakuya <cloud2000s@gmail.com>
2020-12-18 03:17:04 +0800

    modified 541.cpp add new method

--- Cpp/541_Reverse_String_II.cpp
+++ Cpp/541_Reverse_String_II.cpp
@@ -2,28 +2,31 @@
 #include <string>
 using namespace std;
 
-class Solution {
-   public:
-    string reverseStr(string s, int k) {
-        return s;
+string reverseStr(string s, int k) {
+    for (int i = 0; i < s.size(); i += 2 * k) {
+        // 另一種解法只要一行即可
+        // reverse(s.begin() + i, min(s.begin() + i + k, s.end()));
+        int l = i, r;
+        // i+k-1如果大於s.size()-1，則右邊的pointer r = s.size()-1
+        (i + k - 1 > s.size() - 1) ? r = s.size() - 1 : r = i + k - 1;
+        while (r > l) {
+            swap(s[l], s[r]);
+            l++;
+            r--;
+        }
     }
-};
+    return s;
+}
+/*
 string reverseStr(string s, int k) {
     for (int i = 0; i < s.length(); i++) {
         if (i % (2 * k) == 0) {
-            // cout << "i = " << i << endl;
             if (i + k > s.length()) {
-                // cout << "i = " << i << endl;
-                // cout << "i+k = " << i + k << endl;
                 // 這邊有問題 修改l < (s.length() / 2) 邊界問題！！！
                 for (int l = i; l < i + (s.length() - i) / 2; l++) {
-                    // cout << "l = " << l << endl;
                     char tmp = s[l];
                     s[l] = s[s.length() + i - 1 - l];
                     s[s.length() + i - 1 - l] = tmp;
-                    // cout << "#######################################" << endl;
-                    // cout << s.length() + i - 1 - l << endl;
-                    // cout << s[s.length() + i - 1 - l] << endl;
                 }
                 break;
             } else if (i + k - 1 < s.length()) {
@@ -38,22 +41,14 @@ string reverseStr(string s, int k) {
     }
     return s;
 }
-
+*/
 int main() {
     string s = "hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl";
     int k = 39;
     string rev;
     rev = reverseStr(s, k);
 
-    // for (int i = 0; i < s.length(); i++) {
-    //     if (i % 2 == 0) {
-    //         char tmp;
-    //         tmp = s[i];
-    //         s[i] = s[i + 1];
-    //         s[i + 1] = tmp;
-    //     }
-    // }
-    cout << s.length() << endl;
+    // cout << s.length() << endl;
 
     cout << rev << endl;
 
