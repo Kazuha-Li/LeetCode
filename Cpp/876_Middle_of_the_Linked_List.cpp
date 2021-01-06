4b293cc69d25d14d4f710948123d72b574fbc840
Sakuya <cloud2000s@gmail.com>
2021-01-06 17:46:02 +0800

    add 876.cpp and description

---
+++ Cpp/876_Middle_of_the_Linked_List.cpp
@@ -0,0 +1,62 @@
+/*
+https://leetcode.com/problems/middle-of-the-linked-list/
+
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+Example 2:
+
+Input: [1,2,3,4,5,6]
+Output: Node 4 from this list (Serialization: [4,5,6])
+Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Note:
+
+The number of nodes in the given list will be between 1 and 100.
+
+ref:
+https://leetcode.com/problems/middle-of-the-linked-list/solution/
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+   public:
+    // 利用fast、slow兩個指針
+    // fast 每次比 slow 多走一格
+    // Time = O(n)
+    // Space = O(1)
+    ListNode* middleNode(ListNode* head) {
+        ListNode *fast = head, *slow = head;
+        while (fast == NULL || fast->next == NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
+
+int main() {
+    return 0;
+}
