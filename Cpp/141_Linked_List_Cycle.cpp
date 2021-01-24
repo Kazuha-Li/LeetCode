87e2873b5560f354a817727a64610ca527a44eca
Sakuya <cloud2000s@gmail.com>
2021-01-24 22:05:14 +0800

    add 141_Linked_List_Cycle.cpp and description

---
+++ Cpp/141_Linked_List_Cycle.cpp
@@ -0,0 +1,78 @@
+/*
+https://leetcode.com/problems/linked-list-cycle/
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ 
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+ref:
+https://leetcode.com/problems/linked-list-cycle/solution/
+https://www.cnblogs.com/grandyang/p/4137187.html
+
+*/
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+using namespace std;
+
+//  Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+   public:
+    // 利用快慢雙指針，fast每回合比slow多走一步
+    // 當fast == slow代表出現cycle
+    // Time = O(n)
+    // Space = O(1)
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head, *slow = head;
+        // 注意while條件
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    return 0;
+}
