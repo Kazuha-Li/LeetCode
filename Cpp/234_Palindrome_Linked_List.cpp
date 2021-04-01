9d6b2ed003546e9cf8c4fc7dd3fb465d63d134d4
Sakuya <cloud2000s@gmail.com>
2021-04-02 01:37:25 +0800

    modified and added new method to 234_Palindrome_Linked_List.cpp

--- Cpp/234_Palindrome_Linked_List.cpp
+++ Cpp/234_Palindrome_Linked_List.cpp
@@ -19,7 +19,8 @@ https://www.cnblogs.com/grandyang/p/4635425.html
 
 */
 
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
+
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -31,7 +32,7 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution {
+class Solution_reverse_second_half {
    public:
     // 解法二
     // 先找出 LinkedList 的中點，注意奇數偶數！
@@ -62,7 +63,39 @@ class Solution {
         }
         return true;
     }
-    /*
+};
+class Solution_reverse_second_half_II {
+   public:
+    // 方法同上，但是不需要判斷List 是 odd or even
+    bool isPalindrome(ListNode *head) {
+        if (!head) return true;
+        ListNode *fast = head, *slow = head;
+        // 這裡while裡面的條件如果List長度是odd會停在中間
+        // even的話會停在兩個中間較前面的那一個
+        while (fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        // reverse
+        fast = slow->next;
+        ListNode *newHead = NULL, *t;
+        while (fast) {
+            t = fast->next;
+            fast->next = newHead;
+            newHead = fast;
+            fast = t;
+        }
+        t = head;
+        while (newHead) {
+            if (t->val != newHead->val) return false;
+            newHead = newHead->next;
+            t = t->next;
+        }
+        return true;
+    }
+};
+class Solution_stack {
+   public:
     // 解法一
     // 利用 stack 以及快慢指針，將中間以前的元素依序push進去stack
     // 後半段每個與stack的top元素比較
@@ -85,7 +118,56 @@ class Solution {
         }
         return true;
     }
-    */
+};
+
+class Solution_stack_II {
+   public:
+    // 同上，不同之處在於push進stack的是後半段，不用判斷List是 odd 還是 even
+    bool isPalindrome(ListNode *head) {
+        if (!head) return true;
+        ListNode *fast = head, *slow = head;
+        // 這裡while裡面的條件如果List長度是odd會停在中間
+        // even的話會停在兩個中間較前面的那一個
+        while (fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        stack<ListNode *> st;
+        while (slow->next) {
+            st.push(slow->next);
+            slow = slow->next;
+        }
+        fast = head;
+        while (!st.empty()) {
+            if (fast->val != st.top()->val) return false;
+            st.pop();
+            fast = fast->next;
+        }
+        return true;
+    }
+};
+
+class Solution_vector {
+   public:
+    // output到vector然後複製一份之後reverse
+    // Time = O(n)
+    // Spce = O(n)
+    bool isPalindrome(ListNode *head) {
+        if (!head) return true;
+        ListNode *t = head;
+        vector<int> res;
+        while (t) {
+            res.push_back(t->val);
+            t = t->next;
+        }
+        vector<int> revRes(res.begin(), res.end());
+        reverse(revRes.begin(), revRes.end());
+
+        for (int i = 0; i < res.size(); i++) {
+            if (res[i] != revRes[i]) return false;
+        }
+        return true;
+    }
 };
 
 int main(int argc, char const *argv[]) {
