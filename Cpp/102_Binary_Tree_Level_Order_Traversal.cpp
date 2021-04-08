d56118f6a4db807224a7a5563fc23c6db92ff053
Sakuya <cloud2000s@gmail.com>
2021-04-08 15:57:39 +0800

    modified and added new method to 102_Binary_Tree_Level_Order_Traversal.cpp

--- Cpp/102_Binary_Tree_Level_Order_Traversal.cpp
+++ Cpp/102_Binary_Tree_Level_Order_Traversal.cpp
@@ -1,10 +1,33 @@
 /*
 https://leetcode.com/problems/binary-tree-level-order-traversal/
 
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
 
 ref:
 https://www.cnblogs.com/grandyang/p/4051321.html
 https://www.geeksforgeeks.org/level-order-tree-traversal/
+
 */
 #include <algorithm>
 #include <iostream>
@@ -22,11 +45,11 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution_Iteration {
    public:
     // BFS
     // Time = O(n)
-    // 還有一種遞迴解法 https://www.cnblogs.com/grandyang/p/4051321.html
+    // Space = O(n)
     vector<vector<int>> levelOrder(TreeNode *root) {
         vector<vector<int>> ans;
         if (!root) return ans;
@@ -37,14 +60,34 @@ class Solution {
             vector<int> level;
             for (int i = 0; i < n; i++) {
                 TreeNode *top = q.front();
-                level.push_back(top->val);
                 q.pop();
+                level.push_back(top->val);
                 if (top->left) q.push(top->left);
                 if (top->right) q.push(top->right);
             }
             ans.push_back(level);
         }
         return ans;
+    }
+};
+
+class Solution_recursive {
+   public:
+    // Time = O(n)
+    // Space = O(n)
+    // 因為 res 的 index 是從 0 開始， 所以當 res.size() == level 時
+    // 要重新申請一個新的 empty vector {}
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        vector<vector<int>> res;
+        helper(root, res, 0);
+        return res;
+    }
+    void helper(TreeNode *root, vector<vector<int>> &res, int level) {
+        if (!root) return;
+        if (res.size() == level) res.push_back({});
+        helper(root->left, res, level + 1);
+        res[level].push_back(root->val);
+        helper(root->right, res, level + 1);
     }
 };
 
