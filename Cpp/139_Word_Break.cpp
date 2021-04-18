e540074bb5722a773e1dbf357cf1969af1c395bc
Sakuya <cloud2000s@gmail.com>
2021-04-18 23:41:38 +0800

    modified 139_Word_Break.cpp

--- Cpp/139_Word_Break.cpp
+++ Cpp/139_Word_Break.cpp
@@ -40,36 +40,57 @@ https://github.com/grandyang/leetcode/issues/139
 #include <iostream>
 using namespace std;
 
-class Solution {
+class Solution_brute_force_recursive {
    public:
-    unordered_set<string> dict;
-    vector<int> memo;
-    bool wordBreak_recursive(string s, vector<string>& wordDict) {
-        for (auto w : wordDict) dict.insert(w);
-        memo.resize(s.size(), -1);
-        return helper(s, wordDict, 0);
+    // TLE
+    // start 紀錄 substr 起點
+    // 將string 切成兩段，前半段存在wordDict && 後半段存在wordDict才是 true
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        return helper(s, wordDict, dict, 0);
     }
-    bool helper(string s, vector<string>& wordDict, int start) {
-        if (start == s.size()) return true;
-        if (memo[start] != -1) {
-            cout << "start = " << start << endl;
-            return memo[start];
+    bool helper(string& s, vector<string>& wordDict, unordered_set<string>& dict, int start) {
+        if (start == (int)s.size()) return true;
+        for (int i = start; i < (int)s.size(); i++) {
+            if (dict.count(s.substr(start, i - start + 1)) && helper(s, wordDict, dict, i + 1)) {
+                return true;
+            }
         }
-        for (int i = 1; i <= s.size() - start; i++) {
-            // cout<<s.substr(start, i)<<endl;
-            if (dict.count(s.substr(start, i)) && helper(s, wordDict, start + i)) return memo[start] = 1;
-        }
-        cout << "start = " << start << ", memo[start] = " << memo[start] << endl;
-        return memo[start] = 0;
+        return false;
     }
 };
-class Solution2 {
+class Solution_recursive_with_memo {
+    vector<int> memo;
+    // 使用memo來存中間狀態
    public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        vector<bool> dp(s.size() + 1, false);
+        memo.resize(s.size(), -1);  // 初始化
+        return helper(s, wordDict, dict, 0);
+    }
+    bool helper(string& s, vector<string>& wordDict, unordered_set<string>& dict, int start) {
+        if (start == (int)s.size()) return true;
+        // 如果已經有結果就直接回傳
+        if (memo[start] != -1) return memo[start];
+        for (int i = start; i < (int)s.size(); i++) {
+            if (dict.count(s.substr(start, i - start + 1)) && helper(s, wordDict, dict, i + 1)) return memo[start] = 1;
+        }
+        return memo[start] = 0;
+    }
+};
+
+class Solution2 {
+   public:
+    // Time = O(n^3) -> substr time = O(n)
+    // Space = O(n)
+    // dp[i] = [0,i) 內的子字串是否可拆 = [0,j) to [j,i) = dp[j] && [j,i)
+    // dp.size() == n + 1 因為初始化空字串為true
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        int n = (int)s.size();
+        vector<bool> dp(n + 1, false);
         dp[0] = true;
-        for (int i = 1; i <= s.size(); i++) {
+        for (int i = 1; i < n + 1; i++) {
             // for(int j = 0; j < i; j++){
             for (int j = i - 1; j >= 0; j--) {
                 // because dp.size() = s.size() + 1, so substr(j,i-1) 不用 j+1
@@ -82,140 +103,3 @@ class Solution2 {
         return dp.back();
     }
 };
-
-class Solution3 {
-   public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        if (wordDict.size() == 0) return false;
-
-        unordered_set<string> d(wordDict.begin(), wordDict.end());
-
-        // dp[i] is true only if a valid word or sequence of words ends at i
-        vector<bool> dp(s.size() + 1, false);
-        dp[0] = true;
-
-        for (int i = 1; i <= s.size(); ++i) {
-            for (int j = i - 1; j >= 0; --j) {
-                // check only if a valid sequence of words (or a word) ends at j
-                if (dp[j]) {
-                    const string sub = s.substr(j, i - j);
-                    if (d.count(sub)) {
-                        // Ending at i is a valid word
-                        dp[i] = true;
-                        // Others j values might be false
-                        // We stop here since there is one valid sequence ending here
-                        break;
-                    }
-                }
-            }
-        }
-        return dp[s.size()];
-    }
-};
-/*class Solution
-{
-  public:
-     bool wordBreak(string s, vector<string>& wordDict)
-     {
-         if(wordDict.size() == 0)
-         {
-             return false;
-         }
-         
-         unordered_set<string> d(wordDict.begin(), wordDict.end());
-         vector<bool> dp(s.length()+1 , false);
-         dp[0]=true;
-         
-         for(int i=0;i<=s.length();i++)
-         {
-             for(int j=0;j<i;j++)
-             {
-                // check only if a valid sequence of words (or a word) ends at j
-                if(dp[j]) 
-                {
-                    string sub = s.substr(j, i);
-                    if(d.count(sub))
-                    {
-                        // Ending at i is a valid word
-                        dp[i]=true;
-                       // Others j values might be false
-					   // We stop here since there is one valid sequence ending here
-                       break; 
-                    }
-                }
-             }
-         }
-         return dp[s.length()];
-     }
-};
-*/
-
-/*
-//memoization
-class Solution 
-{
-    public:
-    bool helper(const string& s, int start, const unordered_set<string>& d, vector<int>& memo) 
-    {
-        if (start == s.size()) 
-        {
-            return true;
-        }
-        
-        if (memo[start] != -1) return memo[start];
-        
-        for (int i = start + 1; i <= s.size(); ++i) 
-        {
-            const string sub = s.substr(start, i - start); 
-            if (d.count(sub) == 1) {
-                if (helper(s, i, d, memo)) {
-                    memo[start] = 1;
-                    return true;
-                }
-            } 
-        }
-        
-        memo[start] = 0;
-        return false;
-    }
-    
-    bool wordBreak(string s, vector<string>& wordDict)
-    {
-        unordered_set<string> d(wordDict.begin(), wordDict.end());
-		// Use a vector since we can index from 0 to N. 
-		// Possible values in vector are: -1, 0, 1.
-        vector<int> memo(s.length(), -1); 
-        return helper(s, 0, d, memo);
-    }
-};
-*/
-//recursive
-/*
-class Solution 
-{
-public:
-     bool wordBreak(string s, vector<string>& wordDict)
-     {
-         int n=s.length();
-         if(s.length() == 0)
-         {
-             return true;
-         }
-         
-         for(auto num: wordDict)
-         {
-             string sub=s.substr(0,num.length());
-             bool result=false;
-             if(sub == num)
-             {
-                 result=wordBreak(s.substr(num.length()),wordDict);
-             }
-             if(result)
-             {
-                 return true;
-             }
-         }
-         return false;
-     }
-};
-*/
